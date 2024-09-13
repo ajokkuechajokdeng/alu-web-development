82395fb4bc436a06fbfbe2a1242630ec6eb62cae
ajokkuechajokdeng <a.deng2@alustudent.com>
2024-09-13 23:17:15 +0200

    remove space

--- caching/100-lfu_cache.py
+++ caching/100-lfu_cache.py
@@ -48,4 +48,3 @@ class LFUCache(BaseCaching):
                     self.queue.insert(self.queue.index(key) + 1,
                                       self.queue.pop(self.queue.index(key)))
         return self.cache_data.get(key)
-    
